e2da740ed987dd7223b014af236b94fae48c7377
Rahil Patel <patelrahil15@gmail.com>
2023-02-25 16:05:25 -0600

    Multiplayer

--- Game.py
+++ Game.py
@@ -9,7 +9,7 @@ PURPLE = (216,191,216)
 FPS = 60
 
 PLAYER1_IMG = pygame.image.load(os.path.join('ASSETS', 'sPlayerIdle_strip4.png'))
-PLAYER2_IMG = pygame.image.load(os.path.join('ASSETS', 'sEnemy_strip7.png'))
+#PLAYER2_IMG = pygame.image.load(os.path.join('ASSETS', 'sEnemy_strip7.png'))
 
 def draw_window():
     WIN.fill(PURPLE)
